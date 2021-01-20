71154d52f35e25d0fb8acd1a19b868b246c4156e
Nahid Kalam <44406196+akatahin@users.noreply.github.com>
2021-01-20 18:24:10 -0500

    goal 1 started

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import React, { Component, useState } from "react";
+import { textChangeRangeIsUnchanged } from "typescript";
 import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  let questionNum = 0;
+  return <Question text={data[questionNum]} />
+}
+
+function Question(props){
+  return <div>{props.text}</div>;
 }
 
 export default App;
