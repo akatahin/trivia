cb31693f067756090a4772dedc75a508faa6c0e4
Nahid Kalam <44406196+akatahin@users.noreply.github.com>
2021-02-03 18:23:34 -0500

    Goal 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,44 @@ import { textChangeRangeIsUnchanged } from "typescript";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+const nestedHtml = 
+  `<div>
+    <p> Hello! </p>
+    <p> Goodbye! </p>
+    <p> See ya! </p>
+  </div>`
+
 function App() {
-  let questionNum = 1;
-  return <Question text={data[questionNum].question.text} />
+  let questionNum = 0;
+  return (
+    <div>
+      <Question text={data[questionNum].question.text} choice={data[questionNum].question.choices} />
+
+      <NextQuestion /> 
+    </div>
+  )
 }
 
 function Question(props){
-  return <div>{props.text}</div>;
+  return (
+  <div>
+    <div>{props.text}</div>
+    {
+      props.choice.map(ansChoice => {
+        return <Answer ans={ansChoice} /> 
+    })
+    }
+  </div>
+)
+}
+
+function NextQuestion(){ 
+ return <button>Next Question</button> 
+ 
+}
+
+function Answer(props){
+  return <p>{props.ans}</p>
 }
 
 export default App;
